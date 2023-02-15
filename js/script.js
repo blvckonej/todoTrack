1467e5c0870ed817f2eae212f1081ad85b22c62d
Maxim Razin <blvckonej@gmail.com>
2023-02-15 21:46:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function submitForm() {
+    const input = document.querySelector('.input').ariaValueMax;
+    if (!input) {
+        return;
+    }
+
+    document.querySelector
+}
